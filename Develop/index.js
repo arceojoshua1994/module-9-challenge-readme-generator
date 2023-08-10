5603c1dcc917f2361720bbd54672837b2738e673
arceojoshua94 <arceojoshua94@outlook.com>
2023-08-10 19:59:51 -0400

    add application logic into develop branch

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,103 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Add prompt for the user to gather information needed for the README file
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Enter your project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a project description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ])
+  .then(answers => {
+    // Generate the README content based on user input
+    const readmeContent = `
+# ${answers.projectTitle}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${answers.description}
 
-// Function call to initialize app
-init();
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+![License Badge](https://img.shields.io/badge/license-${encodeURIComponent(answers.license)}-blue)
+This project is covered under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For additional questions, you can reach me through GitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})
+Contact me via email at ${answers.email}.
+    `;
+
+    // Write the README content to a file
+    fs.writeFile('README.md', readmeContent, err => {
+      if (err) {
+        console.error('Error writing README file:', err);
+      } else {
+        console.log('README file generated successfully!');
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
