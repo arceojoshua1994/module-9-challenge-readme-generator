a5843ed3313c2e383653fffa8d6ce4471abe3f3a
arceojoshua94 <arceojoshua94@outlook.com>
2023-08-17 21:54:24 -0400

    push to main and add readme video link

--- Main/index.js
+++ Main/index.js
@@ -69,4 +69,107 @@ function init() {
   });
 }
 
-init();
+init();const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Note: The following code adds a prompt for the user to gather information needed for the README file
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Enter your project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a project description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ])
+  .then(answers => {
+    // Note: The following code generates the README content based on user input
+    const readmeContent = `
+# ${answers.projectTitle}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+![License Badge](https://img.shields.io/badge/license-${encodeURIComponent(answers.license)}-blue)
+This project is covered under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For additional questions, you can reach me through GitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})
+Contact me via email at ${answers.email}.
+    `;
+
+    // Note: The following code (writeFile) writes the README content to create a file containing the collected information and adds the info to the template you created above
+    fs.writeFile('README.md', readmeContent, err => {
+      if (err) {
+        console.error('Error writing README file:', err);
+      } else {
+        console.log('README file generated successfully!');
+      }
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# module-9-challenge-readme-generator
+# Command-Line README Generator
+
+## Description
+This is a command-line application that generates a professional README file based on user input using the Inquirer package.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+1. Clone this repository to your local machine.
+2. Run 'npm install' to install the required dependencies.
+
+## Usage
+1. Open your terminal.
+2. Navigate to the project directory.
+3. Run 'node index.js' to start the application.
+4. Answer the prompts to provide information for the README file.
+5. Once all prompts are answered, the README file will be generated in the project directory.
+
+[Video Walkthrough](https://drive.google.com/file/d/1w9ehAIw3m4PdERWv74eyhOn8oLSZsF40/view?usp=share_link)
+
+
+
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](./LICENSE) file for details.
+
+## Contributing
+Contributions are welcome! Please refer to the [CONTRIBUTING](./CONTRIBUTING.md) guidelines for more information.
+
+## Tests
+To run tests, use the following command:
+\`\`\`
+npm test
+\`\`\`
+
+## Questions
+If you have any questions or need assistance, feel free to contact me:
+- GitHub: [YourGitHubUsername](https://github.com/YourGitHubUsername)
+- Email: your.email@example.com
